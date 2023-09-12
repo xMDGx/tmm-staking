422cc19dde6a129fb20151b228c9007d540f8567
TrickMyMind <mastermind@trickmymind.com>
2023-09-12 19:52:50 -0400

    Prelim setup from scratch.

--- programs/tmm-staking/src/lib.rs
+++ programs/tmm-staking/src/lib.rs
@@ -1,15 +1,132 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{self, Mint, Token, TokenAccount, Transfer, transfer},
+};
+
+use solana_program::clock::Clock;
 
 declare_id!("2oXApx9k2sPsCdDbQRhbxwzQxm4nrVz3SXK1CorL1FhL");
+
+pub mod constants {
+    pub const TOKEN_SEED: &[u8] = b"token";
+    pub const STAKE_INFO_SEED: &[u8] = b"stake_info";
+}
 
 #[program]
 pub mod tmm_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn stake(ctx: Context<Stake>, amount: u64) -> Result<()> {
+        let stake_info = &mut ctx.accounts.stake_info_account;
+
+        if stake_info.total_stake > 0.0 {
+            return Err(ErrorCode::IsStaked.into());
+        }
+
+        if amount <= 0 {
+            return Err(ErrorCode::NotEnough.into());
+        }
+
+        let clock = Clock::get()?;
+
+        stake_info.stake_at_slot = clock.slot;
+        stake_info.earned_stake = 0 as f64;
+        stake_info.donated_stake = 0 as f64;
+        stake_info.target_days = 0;
+        stake_info.actual_days = 0;
+
+        let total_stake = (amount)
+            .checked_mul(10u64.pow(ctx.accounts.mint.decimals as u32))
+            .unwrap();
+        // let total_stake = amount as f64 / 10f64.powi(6);
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.user_token_account.to_account_info(),
+                    to: ctx.accounts.stake_account.to_account_info(),
+                    authority: ctx.accounts.signer.to_account_info(),
+                },
+            ),
+            total_stake,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn unstake(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Stake<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::TOKEN_SEED, signer.key().as_ref()],
+        bump,
+        payer = signer,
+        token::mint = mint,
+        token::authority = stake_account,
+    )]
+    pub stake_account: Account<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::STAKE_INFO_SEED, signer.key().as_ref()],
+        bump,
+        payer = signer,
+        // space = 8 + 8 + 8 + 8 + 2 + 2,
+        space = 8 + std::mem::size_of::<StakeInfo>(),
+    )]
+    pub stake_info_account: Account<'info, StakeInfo>,
+    
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub user_token_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct StakeInfo {
+    pub stake_at_slot: u64,
+    pub total_stake: f64,
+    pub earned_stake: f64,
+    pub donated_stake: f64,
+    pub target_days: u16,
+    pub actual_days: u16,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Staking exists already.")]
+    IsStaked,
+    #[msg("Staking does not exist.")]
+    NotStaked,
+    #[msg("Staking has not unlocked yet.")]
+    StakeLocked,
+    #[msg("Not enough to stake.")]
+    NotEnough,
+}
--- tests/tmm-staking.ts
+++ tests/tmm-staking.ts
@@ -1,16 +1,72 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TmmStaking } from "../target/types/tmm_staking";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+
 
 describe("tmm-staking", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  console.log("Starting test...");
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
+  const connection = new Connection("http://localhost:8899", "confirmed");
 
   const program = anchor.workspace.TmmStaking as Program<TmmStaking>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let mintKeyPair = anchor.web3.Keypair.generate();
+
+  it("stake", async () => {
+    let [stakeAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("token")],
+      program.programId,
+    );
+
+    await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      payer.publicKey,
+    );
+
+    let [stakeInfo] = PublicKey.findProgramAddressSync(
+      [Buffer.from("stake_info")],
+      program.programId,
+    );
+
+    let userTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      payer.publicKey,
+    );
+
+    await mintTo(
+      connection,
+      payer.payer,
+      mintKeyPair.publicKey,
+      userTokenAccount.address,
+      payer.payer,
+      1000,
+    );
+
+    const txn = await program.methods
+      .stake(new anchor.BN(1))
+      .signers([payer.payer])
+      .accounts({
+        signer: payer.publicKey,
+        stakeAccount: stakeAccount,
+        stakeInfoAccount: stakeInfo,
+        userTokenAccount: userTokenAccount.address,
+        mint: mintKeyPair.publicKey,
+      })
+      .rpc();
+
+    console.log("TxnHash: ", txn);
+
+    // Fetch data from the account.
+    // let accountData = await program.account.stakeInfo.fetch(stakeAccount.publicKey);
+    // console.log('Total Staked: ', accountData.totalStake.toString());
   });
 });
