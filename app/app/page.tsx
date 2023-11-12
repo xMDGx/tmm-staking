566d41b70e8889ade5a4e519351134aae6056ad0
TrickMyMind <mastermind@trickmymind.com>
2023-11-12 18:56:48 -0500

    UI Page set with Wallet Adapter working.

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import { NextPage } from "next"
 import { useState } from "react"
+import { Wallet } from "@/components/Wallet";
 
 const Home: NextPage = () => {
   var [habitId, setHabitId] = useState(0);
   var [depositAmount, setDepositAmount] = useState(0);
+  var [withdrawalAmount, setWithdrawalAmount] = useState(0);
 
   const submitDeposit = async () => {
-    if (habitId <= 0 || depositAmount <= 0) {
-      alert("Deposit amount must be greater than 0");
-      return;
-    }
 
+    return;
+  };
 
+  const submitWithdrawal = async () => {
+
+    return;
   };
 
   return (
@@ -19,6 +24,9 @@ const Home: NextPage = () => {
       <br />
       <br />
       <h1>TrickMyMind Frontend UI Testing</h1>
+      <br />
+      <br />
+      <Wallet />
       <br />
       <br />
       <label>Habit ID:</label>
@@ -29,14 +37,36 @@ const Home: NextPage = () => {
         onChange={(e) => setHabitId(parseInt(e.target.value))}
       />
       <br />
-      <label>USDC Deposit Amount:</label>
+      <label>Deposit Amount (USDC):</label>
       <input
         type="number"
         id="deposit_amount"
         name="deposit_amount"
         onChange={(e) => setDepositAmount(parseInt(e.target.value))}
       />
+      <br />
       <button onClick={() => { submitDeposit }}>Deposit</button>
+      <br />
+      <br />
+      <br />
+      <p>
+        User does activities for 30 days, then can withdraw.  For devnet, its 2 minutes instead of 30 days withdraw
+        manual input of decimal percentage.
+      </p>
+      <br />
+      <br />
+      <br />
+      <label>Percent Completed (decimal):</label>
+      <input
+        type="number"
+        id="withdrawal_amount"
+        name="withdrawal_amount"
+        min="0"
+        max="1"
+        onChange={(e) => setWithdrawalAmount(Number(e.target.value))}
+      />
+      <br />
+      <button onClick={() => { submitWithdrawal }}>Withdraw</button>
     </div>
   )
 };
---
+++ app/components/Wallet.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+// import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+export const Wallet: FC = () => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      // new UnsafeBurnerWalletAdapter(),
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+          { /* Your app's components go here, nested within the context providers. */}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
